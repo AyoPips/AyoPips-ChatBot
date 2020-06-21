552b45b0c135efc57a4da6b32467b05e984275e6
AyoPips <engineerfao@gmail.com>
2020-06-21 16:11:10 +0100

    using git for heroku deployment

---
+++ build/precache-manifest.1f93e3f0ceec49aa501de57a951c04ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966b79ecdb9bf9f862db3e4040c0762d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f666fd5f89c26faebf",
+    "url": "/static/js/2.8770cf0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8770cf0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077e7fc0cd3b03cca63f",
+    "url": "/static/js/main.6a869401.chunk.js"
+  },
+  {
+    "revision": "9196175769b6570a9dea",
+    "url": "/static/js/runtime-main.f88cfdd6.js"
+  }
+]);
